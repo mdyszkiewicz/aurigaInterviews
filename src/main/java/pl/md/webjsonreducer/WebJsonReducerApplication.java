c48c1d409c85fc6df581b029dadf3757da974e91
Mateusz Dyszkiewicz <mateusz@dyszkiewicz.org>
2019-07-28 09:31:28 +0200

    Initial SprinBoot template

---
+++ src/main/java/pl/md/webjsonreducer/WebJsonReducerApplication.java
@@ -0,0 +1,13 @@
+package pl.md.webjsonreducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJsonReducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJsonReducerApplication.class, args);
+	}
+
+}
